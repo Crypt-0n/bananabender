29007feff87848b03aeceff1e312774bbb650ab7
arnoldCrypt-0n <ahebert@crypt-0n.fr>
2018-12-24 17:29:11 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,255 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "main.h"
+
+int main (int argc, char *argv[]){
+    Road road;
+    set_arg(argc, argv, &road);
+    print_title();
+    int cod = get_all(&road);
+    return cod;
+}
+int print_title(){
+    fprintf(stdout,"\"HIVE\",\"KEY\",\"cValues\",\"lpftLastWriteTime\",\"cbSecurityDescriptor\",\"DATA\",\"VALUE\"\n");
+    return 0;
+}
+
+int set_arg(int argc, char *argv[], Road *road){
+    int narg = ( argc - 1 ) / 2 ;
+    for(int n = 1;n<=narg;n++){
+        if (strcmp(argv[(n*2)-1], "-h") == 0){
+            set_hive(argv[(n*2)], road);
+        }else if (strcmp(argv[(n*2)-1], "-k") == 0){
+            strcpy(road->str,argv[(n*2)]);
+        }
+    }
+    return 0;
+}
+
+int set_hive(char *argv, Road *road){
+    if (strcmp(argv, "hkcu") == 0){
+        road->hive=HKEY_CURRENT_USER;
+        strcpy(road->shive, "HKEY_CURRENT_USER");
+    }
+    else if (strcmp(argv, "hklm") == 0){
+        road->hive=HKEY_LOCAL_MACHINE;
+        strcpy(road->shive, "HKEY_LOCAL_MACHINE");
+    }
+    else if (strcmp(argv, "hkcr") == 0){
+        road->hive=HKEY_CLASSES_ROOT;
+        strcpy(road->shive, "HKEY_CLASSES_ROOT");
+    }
+    else if (strcmp(argv, "hkcc") == 0){
+        road->hive=HKEY_CURRENT_CONFIG;
+        strcpy(road->shive, "HKEY_CURRENT_CONFIG");
+    }
+    else if (strcmp(argv, "hku") == 0){
+        road->hive=HKEY_USERS;
+        strcpy(road->shive, "HKEY_USERS");
+    }else {
+        fprintf(stderr," %s hive invalid \n\n",argv );
+        return 1;
+    }
+    return 0;
+}
+
+int get_all(Road *road){
+    KeyInfo keyinfo;
+    int cod=query_key(road,&keyinfo);
+    if(cod==0){
+        if(keyinfo.cValues>0){
+            get_values(road, &keyinfo);
+        }
+        if(keyinfo.cSubKeys>0){
+            get_subKeys(road, &keyinfo );
+        }
+    }else if(cod!=0){
+        //printf("Erreur query_key, retcod : %d \n",cod);
+        //printf("  road->str : %s \n",road->str);
+    }
+    return cod;
+}
+
+int query_key(Road *road, KeyInfo *k){
+    DWORD retCode;
+    if( ( retCode=RegOpenKeyEx( road->hive,road->str, 0, KEY_READ,&k->key))!=0){
+        fprintf(stderr,"Erreur RegOpenKeyEx  \n");
+        if(retCode==5){
+            fprintf(stderr," -> Access is denied (%d) \n",(int)retCode);
+            fprintf(stderr," %s  %s \n\n",road->shive,road->str);
+        }
+    }else{
+    retCode = RegQueryInfoKey(
+        k->key,                    // key handle
+        k->achClass,                // buffer for class name
+        &k->cchClassName,           // size of class string
+        NULL,                    // reserved
+        &k->cSubKeys,               // number of subkeys
+        &k->cbMaxSubKey,            // longest subkey size
+        &k->cchMaxClass,            // longest class string
+        &k->cValues,            // number of values for this key
+        &k->cchMaxValue,            // longest value name
+        &k->cbMaxValueData,         // longest value data
+        &k->cbSecurityDescriptor,   // security descriptor
+        &k->ftLastWriteTime);
+    }
+    RegCloseKey(k->key);
+    if(retCode){
+        fprintf(stderr,"Erreur query_key  (%d) ",(int)retCode);
+        if(retCode==ERROR_INVALID_HANDLE){
+            fprintf(stderr," ERROR_INVALID_HANDLE  (%d) \n",(int)ERROR_INVALID_HANDLE);
+            fprintf(stderr," %s \n\n",road->str);
+        }
+        if(retCode==ERROR_MORE_DATA){
+            fprintf(stderr," ERROR_MORE_DATA (%d) \n",(int)ERROR_MORE_DATA);
+            fprintf(stderr," %s \n\n",road->str);
+        }else{
+            fprintf(stderr," Erreur %d \n",(int)retCode);
+            fprintf(stderr," %s \n\n",road->str);
+        }
+    }
+    return (int)retCode;
+}
+
+int get_values(Road *road, KeyInfo *keyinfo){
+    DWORD retCode;
+    EnumValue enumValue;
+
+    LPSTR valueName[keyinfo->cchMaxValue];
+    enumValue.lpValueName=valueName;
+
+    LPBYTE valueData[keyinfo->cbMaxValueData];
+    enumValue.lpData=valueData;
+
+    make_richkey(keyinfo, road);
+    enumValue.key=keyinfo->key;
+    RegOpenKeyEx( road->hive,road->str, 0, KEY_READ,&enumValue.key);
+
+    for (enumValue.dwIndex=0, retCode=ERROR_SUCCESS; enumValue.dwIndex<keyinfo->cValues; enumValue.dwIndex++){
+        if ( ( retCode = get_data(&enumValue) ) == ERROR_SUCCESS ){
+            fprintf(stdout,"%s",road->richkey);
+            print_data(enumValue);
+        }else {
+            fprintf(stderr,"Erreur %d get_data() : ",(int)retCode );
+            fprintf(stderr,"dwIndex %d/%d  ",(int)enumValue.dwIndex,(int)keyinfo->cValues );
+            if(retCode==ERROR_MORE_DATA){
+                fprintf(stderr,"ERROR_MORE_DATA : maxvalue : %d\n",(int)keyinfo->cchMaxValue);
+            }
+            fprintf(stderr,"%d(%s)\n\n",(int)keyinfo->cbMaxValueData, road->str);
+        }
+    }
+    RegCloseKey(enumValue.key);
+    return 0;
+}
+
+int get_data(EnumValue *enumValue){
+    DWORD retCode;
+    enumValue->lpcchValueName = (LPDWORD)MAX_VALUE_NAME;
+    enumValue->lpcbData = (LPDWORD)MAX_VALUE_NAME;
+    retCode = RegEnumValue(
+            enumValue->key,
+            enumValue->dwIndex,
+            (LPSTR)enumValue->lpValueName,
+            (LPDWORD)&enumValue->lpcchValueName,
+            NULL,
+            (LPDWORD)&enumValue->lpType,
+            (LPBYTE)enumValue->lpData,
+            (LPDWORD)&enumValue->lpcbData);
+    return retCode;
+}
+
+int make_richkey(KeyInfo *keyinfo, Road *road){
+    SYSTEMTIME stUTC, stLocal;
+    FileTimeToSystemTime(&keyinfo->ftLastWriteTime, &stUTC);
+    SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+    sprintf(road->richkey,"\"%s\",\"%s\",\"%d\",\"%d/%d/%d %dh%d\",\"%d\",",
+            road->shive,
+            road->str ,
+            (int)keyinfo->cValues,
+            stLocal.wDay,stLocal.wMonth,stLocal.wYear,stLocal.wHour, stLocal.wMinute,
+            (int)keyinfo->cbSecurityDescriptor  );
+    return 0;
+}
+
+int print_data(EnumValue enumValue){
+    switch ((int)enumValue.lpType){
+        case REG_SZ://A null-terminated string
+            fprintf(stdout,"\"REG_SZ\",\"%s\",\"%s\"\n",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            break;
+        case REG_BINARY://Binary data in any form
+            fprintf(stdout,"\"REG_BINARY\",\"%s\",\"%d\"\n",(char*)enumValue.lpValueName, (int)*enumValue.lpData);
+            break;
+        case REG_DWORD://A 32-bit number.
+            fprintf(stdout,"\"REG_DWORD\",\"%s\",\"%d\"\n",(char*)enumValue.lpValueName, (int)*enumValue.lpData);
+            break;
+        case REG_DWORD_BIG_ENDIAN://A 32-bit number in big-endian format.
+            fprintf(stdout,"\"REG_DWORD_BIG_ENDIAN\",\"%s\",\"%d\"\n",(char*)enumValue.lpValueName, (int)*enumValue.lpData);
+            break;
+        case REG_EXPAND_SZ://A null-terminated string for example, "%PATH%"
+            fprintf(stdout,"\"REG_EXPAND_SZ\",\"%s\",\"%s\"\n",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            break;
+        case REG_LINK://A null-terminated Unicode string that contains the target path of a symbolic link that was created by calling the RegCreateKeyEx function with REG_OPTION_CREATE_LINK.
+            fprintf(stdout,"\"REG_LINK\",\"%s\",\"%s\"\n",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            break;
+        case REG_MULTI_SZ://A sequence of null-terminated strings, terminated by an empty string (\0).
+            fprintf(stdout,"\"REG_MULTI_SZ\",\"%s\",\"%s\"\n",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            break;
+        case REG_NONE://No defined value type.
+            //printf(" REG_NONE %s: %s ",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            fprintf(stdout,"\"REG_NONE\",\"%s\",\"%s\"\n",(char*)enumValue.lpValueName, (char*)enumValue.lpData);
+            break;
+        case REG_QWORD://A 64-bit
+            fprintf(stdout,"\"REG_QWORD\",\"%s\",\"%d\"\n",(char*)enumValue.lpValueName, (int)*enumValue.lpData);
+            break;
+        default:
+            fprintf(stdout,"\"default\",\"%s\",\"%d\"\n",(char*)enumValue.lpValueName, (int)enumValue.lpData);
+            break;
+        }
+        return 0;
+}
+
+int get_subKeys(Road *road, KeyInfo *keyinfo){
+    KeyEnum key_enum;
+    DWORD retCode=0;
+    Road newRoad;
+    RegOpenKeyEx( road->hive,road->str, 0, KEY_READ, &key_enum.key);
+    key_enum.dwIndex = 0 ;
+    while(key_enum.dwIndex < keyinfo->cSubKeys ){// && retCode==0){
+        key_enum.lpcchName = (LPDWORD)keyinfo->cbMaxSubKey+1;
+        retCode=RegEnumKeyEx(
+            key_enum.key,
+            key_enum.dwIndex,
+            key_enum.lpName,
+            (LPDWORD)&key_enum.lpcchName,
+            NULL,
+            NULL,
+            NULL,
+            &key_enum.lpftLastWriteTime);
+        if( retCode==0){
+            make_key(*road, key_enum, &newRoad);
+            get_all(&newRoad);
+        }else{
+            fprintf(stderr,"Erreur RegEnumKeyEx, retCode : %d \n",(int)retCode);
+            fprintf(stderr,"  keyinfo->cSubKeys : %d \n",(int)keyinfo->cSubKeys);
+            fprintf(stderr,"  key_enum.dwIndex : %d \n",(int)key_enum.dwIndex);
+        }
+    key_enum.dwIndex++;
+    }
+    RegCloseKey(key_enum.key);
+    return EXIT_SUCCESS;
+}
+
+int make_key(Road road, KeyEnum key_enum, Road *newRoad){
+    size_t len= strlen(road.str);
+    if(len==0 ){
+        strcpy(newRoad->str, key_enum.lpName);
+    }else if(len>0){
+        strcpy(newRoad->str, road.str);
+        strcat(newRoad->str, "\\");
+        strcat(newRoad->str, key_enum.lpName);
+    }
+    newRoad->hive=road.hive;
+    strcpy(newRoad->shive, road.shive);
+    return EXIT_SUCCESS;
+}
---
+++ main.h
@@ -0,0 +1,126 @@
+#define MAX_KEY_LENGTH 9999
+#define MAX_VALUE_NAME 9999
+#define STR_MAX  9999
+
+typedef struct Road Road;
+struct Road{
+    HKEY hive;
+    char shive[STR_MAX];// string hive
+    char str[STR_MAX];//string key
+    char richkey[STR_MAX];//string key enrichie "hive","key","security descriptor","last write time",
+};
+
+typedef struct KeyInfo KeyInfo;
+struct KeyInfo{
+    HKEY key;
+    TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
+    DWORD    cbName;                   // size of name string
+    TCHAR    achClass[MAX_PATH] ;  // buffer for class name
+    DWORD    cchClassName ;  // size of class string
+    DWORD    cSubKeys;               // number of subkeys
+    DWORD    cbMaxSubKey;              // longest subkey size
+    DWORD    cchMaxClass;              // longest class string
+    DWORD    cValues;              // number of values for key
+    DWORD    cchMaxValue;          // longest value name
+    DWORD    cbMaxValueData;       // longest value data
+    DWORD    cbSecurityDescriptor; // size of security descriptor
+    FILETIME ftLastWriteTime;      // last write time
+};
+
+typedef struct KeyEnum KeyEnum;
+struct KeyEnum{
+    HKEY      key;     //hive
+    DWORD     dwIndex;  //The index of the subkey to retrieve. This parameter should be zero for the first call to the RegEnumKeyEx function and then incremented for subsequent calls.
+    TCHAR     lpName[MAX_KEY_LENGTH];   //A pointer to a buffer that receives the name of the subkey, including the terminating null character
+    LPDWORD   lpcchName;
+    LPDWORD   lpReserved;   //This parameter is reserved and must be NULL.
+    LPSTR     lpClass[MAX_PATH];;  //A pointer to a buffer that receives the user-defined class of the enumerated subkey. This parameter can be NULL.
+    LPDWORD   lpcchClass;   //A pointer to a variable that specifies the size of the buffer specified by the lpClass parameter, in characters. The size should include the terminating null character. If the function succeeds, lpcClass contains the number of characters stored in the buffer, not including the terminating null character. This parameter can be NULL only if lpClass is NULL.
+    FILETIME lpftLastWriteTime;
+};//ERROR_SUCCESS   ERROR_NO_MORE_ITEMS ERROR_MORE_DATA
+
+typedef struct EnumValue EnumValue;
+struct EnumValue{
+    HKEY    key;
+    DWORD   dwIndex;
+    LPSTR   *lpValueName;
+    LPDWORD lpcchValueName;
+    LPDWORD lpReserved;
+    LPDWORD lpType;
+    LPBYTE  *lpData;
+    LPDWORD lpcbData;
+};
+/**
+    printf() entete du csv
+**/
+int print_title();
+
+/**
+    Recois l'element de argv correspondant a -h
+    initialise la hkey road->hive
+    initialise une chaine de caratere correspondant road->shive
+**/
+int set_hive(char *argv, Road *road);
+
+/**
+    Boucle sur les element de argv
+    appel set_hive()
+    initialise road->str si une clef a ete passer en argument (-k)
+**/
+int set_arg(int argc, char *argv[], Road *road);
+
+
+/**
+    Ouvre la clé contenu dans road
+    Enumere les sous clé et recréer les chemin avec leur nom et road->str
+    Rappel get_all() avec chaque sous clé
+    Ferme la clé
+**/
+int get_subKeys(Road *road, KeyInfo *keyinfo);
+
+/**
+    switch sur le Type de la value
+    printf de la value, du type et des data dans le bon format
+**/
+int print_data(EnumValue enumValue);
+
+/**
+    Converti un FILETIME en SYSTEMETIME
+    Passe de UTC a local
+    creer la clé enrichie dans road->richkey (hive,clé, nombre de value, lastwritetime, security descriptor, )
+**/
+int make_richkey(KeyInfo *keyinfo, Road *road);
+
+/**
+    Recupere data et value de l'index dwIndex de la structure enumValue passé en parametre
+**/
+int get_data(EnumValue *enumValue);
+
+/**
+    Ouvre la clé contenu dans road
+    Recupere ses info(nbr subkeys, nbr values, taille max des nom et key, date de derniere modif... )
+    Ferme la clé
+**/
+int query_key(Road *road, KeyInfo *k);
+
+/**
+    Recois une clé et un hive
+    Appel query_key() pour recuperer les info
+    recupere les value si il y en a
+    recupere les sous clé si il y en a
+**/
+int get_all(Road *road);
+
+/**
+    Ecrit la clé
+    Ecrit sa date de derniere modification
+    Boucle sur les value contenu dans une clé et appel get_data()
+    appel print_data() pour ecrir le resultat
+**/
+int get_values(Road *road, KeyInfo *keyinfo);
+
+/**
+    Construit une sous clé newroad->str avec la clé road.str et la sous clé key_enum.lpName
+**/
+int make_key(const Road road, const KeyEnum key_enum, Road *newRoad);
+
